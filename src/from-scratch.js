d7d4144981b8b8b2d502f9988c794cbc89919337
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-04-18 19:06:15 -0400

    q4

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,24 +1,35 @@
 const helloWorldRegex = (str) => {
   return /Hello world/gi.test(str)
 };
-console.log(helloWorldRegex("Hello world")); // true
-console.log(helloWorldRegex("Hello world?")); // true
-console.log(helloWorldRegex("Hello world, are you there?")); // true
-console.log(helloWorldRegex("hello world")); // true
-console.log(helloWorldRegex("HELLO WORLD!")); // true
-console.log(helloWorldRegex("HeLlO wOrLd!")); // true
-console.log(helloWorldRegex("Sup world!")) // false
-console.log(helloWorldRegex("Where in the world is Carmen?")); // false
-console.log(helloWorldRegex("Hello")); // false
+// console.log(helloWorldRegex("Hello world")); // true
+// console.log(helloWorldRegex("Hello world?")); // true
+// console.log(helloWorldRegex("Hello world, are you there?")); // true
+// console.log(helloWorldRegex("hello world")); // true
+// console.log(helloWorldRegex("HELLO WORLD!")); // true
+// console.log(helloWorldRegex("HeLlO wOrLd!")); // true
+// console.log(helloWorldRegex("Sup world!")) // false
+// console.log(helloWorldRegex("Where in the world is Carmen?")); // false
+// console.log(helloWorldRegex("Hello")); // false
 
 
 const hasAVowel = (str) => { };
 
-const hasCatsOrDogs = (str) => { };
+const hasCatsOrDogs = (str) => {
+  return /cats|dogs/gi.test(str)
+};
+
+console.log(hasCatsOrDogs("Gosh, I love having so many cats!")); // true
+console.log(hasCatsOrDogs("Wow, I have a lot of dogs!")); // true
+console.log(hasCatsOrDogs("Cats rule!")); // true
+console.log(hasCatsOrDogs(("I do not care for that dog."))); // false
+console.log(hasCatsOrDogs(("Cat? No way."))); // false
+console.log(hasCatsOrDogs(("Cat? No, but I have a ton of dogs."))); // true
 
 const hasVowelStart = (str) => { };
 
-const hasPunctuationEnd = (str) => { };
+const hasPunctuationEnd = (str) => {
+  return
+};
 
 const hasNothingOrDigits = (str) => { };
 
